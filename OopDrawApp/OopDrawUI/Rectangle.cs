7facbabdc7d28a7031dd580f91c8fffe3e98ac88
ArchieW1 <archie.williamson1@gmail.com>
2022-03-16 18:50:31 +0000

    Exercise 6

---
+++ OopDrawApp/OopDrawUI/Rectangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+
+namespace OopDrawUI
+{
+    public class Rectangle
+    {
+        public Pen Pen { get; private set; }
+        public int XCoord1 { get; private set; }
+        public int YCoord1 { get; private set; }
+        public int XCoord2 { get; private set; }
+        public int YCoord2 { get; private set; }
+
+        public Rectangle(Pen pen, int xCoord1, int yCoord1, int xCoord2, int yCoord2)
+        {
+            Pen = pen;
+            XCoord1 = xCoord1;
+            YCoord1 = yCoord1;
+            XCoord2 = xCoord2;
+            YCoord2 = yCoord2;
+        }
+        
+        public Rectangle(Pen pen, int xCoord1, int yCoord1) 
+            : this(pen, xCoord1, yCoord1, xCoord1, yCoord1)
+        {
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            int xCoord = Math.Min(XCoord1, XCoord2);
+            int yCoord = Math.Min(YCoord1, YCoord2);
+            int width = Math.Min(XCoord1, XCoord2) - xCoord;
+            int height = Math.Min(YCoord1, YCoord2) - yCoord;
+            graphics.DrawRectangle(Pen, xCoord, yCoord, width, height);
+        }
+        
+        public void GrowTo(int newXCoord2, int newYCoord2)
+        {
+            XCoord2 = newXCoord2;
+            YCoord2 = newYCoord2;
+        }
+    }
+}
