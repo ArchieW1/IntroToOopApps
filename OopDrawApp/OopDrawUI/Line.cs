6bb9b4911e83fcbc1f6557ee2a4dcd95d86c85ea
ArchieW1 <archie.williamson1@gmail.com>
2022-03-16 16:01:54 +0000

    Exercise 4

---
+++ OopDrawApp/OopDrawUI/Line.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+
+namespace OopDrawUI
+{
+    public class Line
+    {
+        public Pen Pen { get; private set; }
+        public int XCoord1 { get; private set; }
+        public int YCoord1 { get; private set; }
+        public int XCoord2 { get; private set; }
+        public int YCoord2 { get; private set; }
+
+        public Line(Pen pen, int xCoord1, int yCoord1, int xCoord2, int yCoord2)
+        {
+            Pen = pen;
+            XCoord1 = xCoord1;
+            YCoord1 = yCoord1;
+            XCoord2 = xCoord2;
+            YCoord2 = yCoord2;
+        }
+
+        public Line(Pen pen, int xCoord1, int yCoord1) 
+            : this(pen, xCoord1, yCoord1, xCoord1, yCoord1)
+        {
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            graphics.DrawLine(Pen, XCoord1, YCoord1, XCoord2, YCoord2);
+        }
+
+        public void GrowTo(int newXCoord2, int newYCoord2)
+        {
+            XCoord2 = newXCoord2;
+            YCoord2 = newYCoord2;
+        }
+    }
+}
--- OopDrawApp/OopDrawUI/OopDrawForm.cs
+++ OopDrawApp/OopDrawUI/OopDrawForm.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace OopDrawUI
@@ -9,6 +11,7 @@ namespace OopDrawUI
         private bool _dragging = false;
         private Point _startOfDrag = Point.Empty;
         private Point _lastMousePosition = Point.Empty;
+        private List<Line> _lines = new List<Line>();
         
         public OopDrawForm()
         {
@@ -19,7 +22,10 @@ namespace OopDrawUI
         private void CanvasPictureBox_Paint(object sender, PaintEventArgs e)
         {
             Graphics graphics = e.Graphics;
-            graphics.DrawLine(_currenPen, _startOfDrag, _lastMousePosition);
+            foreach (Line line in _lines)
+            {
+                line.Draw(graphics);
+            }
         }
 
         private void CanvasPictureBox_MouseDown(object sender, MouseEventArgs e)
@@ -27,12 +33,15 @@ namespace OopDrawUI
             _dragging = true;
             _startOfDrag = e.Location;
             _lastMousePosition = e.Location;
+            _lines.Add(new Line(_currenPen, e.X, e.Y));
         }
 
         private void CanvasPictureBox_MouseMove(object sender, MouseEventArgs e)
         {
             if (_dragging)
             {
+                Line currentLine = _lines.Last();
+                currentLine.GrowTo(e.X, e.Y);
                 _lastMousePosition = e.Location;
                 Refresh();
             }
