096823536d337b46870c87cf6afeae03e7134759
ArchieW1 <archie.williamson1@gmail.com>
2022-03-22 19:04:18 +0000

    Exercise 17

--- OopDrawApp/OopDrawUI/Line.cs
+++ OopDrawApp/OopDrawUI/Line.cs
@@ -4,17 +4,8 @@ namespace OopDrawUI
 {
     public class Line : Shape
     {
-        public Line(Pen pen, int xCoord1, int yCoord1, int xCoord2, int yCoord2)
-        {
-            Pen = pen;
-            XCoord1 = xCoord1;
-            YCoord1 = yCoord1;
-            XCoord2 = xCoord2;
-            YCoord2 = yCoord2;
-        }
-
         public Line(Pen pen, int xCoord1, int yCoord1) 
-            : this(pen, xCoord1, yCoord1, xCoord1, yCoord1)
+            : base(pen, xCoord1, yCoord1, xCoord1, yCoord1)
         {
         }
 
--- OopDrawApp/OopDrawUI/Shape.cs
+++ OopDrawApp/OopDrawUI/Shape.cs
@@ -9,6 +9,16 @@ namespace OopDrawUI
         public int YCoord1 { get; protected set; }
         public int XCoord2 { get; protected set; }
         public int YCoord2 { get; protected set; }
+        
+        public Shape(Pen pen, int xCoord1, int yCoord1, int xCoord2, int yCoord2)
+        {
+            Pen = pen;
+            XCoord1 = xCoord1;
+            YCoord1 = yCoord1;
+            XCoord2 = xCoord2;
+            YCoord2 = yCoord2;
+        }
+        
         public abstract void Draw(Graphics graphics);
         public void GrowTo(int newXCoord2, int newYCoord2)
         {
